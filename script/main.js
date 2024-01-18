5b3b98565eef9b8adba6e2b2676f53702ff8515e
amave423 <artem123antropov@mail.ru>
2024-01-18 15:23:44 +0500

    добавил на все страницы меню

--- script/main.js
+++ script/main.js
@@ -1,4 +1,34 @@
 function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('sidebar-expanded');
+    var sidebar = document.getElementById('sidebar');
+    var list = document.getElementById('list');
+    var expanded = sidebar.classList.toggle('sidebar-expanded');
+
+    list.setAttribute('aria-hidden', !expanded);
+
+    var links = document.querySelectorAll('.menu-link');
+    links.forEach(function (link) {
+        if (expanded) {
+            link.setAttribute('href', link.getAttribute('data-href'));
+        } else {
+            link.setAttribute('data-href', link.getAttribute('href'));
+            link.removeAttribute('href');
+        }
+    });
 }
+
+function preventLink(event) {
+    event.preventDefault();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.querySelectorAll('.menu-link');
+    links.forEach(function (link) {
+        link.addEventListener('click', function (event) {
+            if (document.getElementById('sidebar').classList.contains('sidebar-expanded')) {
+                return true;
+            } else {
+                event.preventDefault();
+            }
+        });
+    });
+});
