3391b75d793a140ac0145448bbd8bed7a442f76e
amave423 <artem123antropov@mail.ru>
2024-01-18 14:46:48 +0500

    переделал навигацию

--- script/main.js
+++ script/main.js
@@ -1,21 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.querySelector('.button');
-    var menu = document.querySelector('.menu');
-    var menuLinks = document.querySelectorAll('.menu-link');
-
-    function toggleMenu() {
-        button.classList.toggle('active');
-
-        if (button.classList.contains('active')) {
-            button.setAttribute('aria-expanded', 'true');
-            menu.setAttribute('aria-hidden', 'false');
-            menuLinks.forEach(link => link.setAttribute('tabindex', '0'));
-        } else {
-            button.setAttribute('aria-expanded', 'false');
-            menu.setAttribute('aria-hidden', 'true');
-            menuLinks.forEach(link => link.setAttribute('tabindex', '-1'));
-        }
-    }
-
-    button.addEventListener('click', toggleMenu);
-});
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('sidebar-expanded');
+}
